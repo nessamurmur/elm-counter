9218598117f769a9eeb2166caa089510cacb9b18
Nate West <natescott.west@gmail.com>
2015-11-24 16:24:43 -0800

    Coutner

--- src/main.elm
+++ src/main.elm
@@ -1,6 +1,26 @@
 module Main where
-
+import Signal
 import Html exposing (..)
+import Html.Events exposing (..)
+import StartApp.Simple as App
 
-main : Html
-main = div [] [text "Hello World"]
+type alias Model = Int
+type Action = Increment | Decrement
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Increment -> model + 1
+    Decrement -> model - 1
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div []
+    [ button [ onClick address Decrement ] [ text "-" ]
+    , div []  [ text (toString model) ]
+    , button [ onClick address Increment ] [ text "+" ]
+    ]
+
+main : Signal Html
+main =
+  App.start { model = 0, update = update, view = view }
